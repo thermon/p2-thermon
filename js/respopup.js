8dfc13bc0f2834ece203f4fa20475258c30c887b
thermon <thermon@colinux.(none)>
2009-10-13 22:43:07 +0900

    参照元レス展開時の処理がおかしかったので修正。

--- js/respopup.js
+++ js/respopup.js
@@ -80,7 +80,7 @@ function insertResAll(anchors,button) {
 			importElement=copyHTML(""+importId);
 	//		x=importElement.match(/display:\s*none;?/);
 	//		alert(x+"を見つけました");
-			importElement=importElement.replace(/class="invisible\s+expandAll"/,'class="expandAll"');
+		importElement=importElement.replace(/class="invisible\s+(expand(Single|All)[^"]*)"/g,'class="$1"');
 
 			//参照先レス情報をコピー
 			resdiv=document.createElement('blockquote');
